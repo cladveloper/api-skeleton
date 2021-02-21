f8d6c7de2223fb119f7b2fdccfa5f6f2fa2d9132
unknown <claudio.skdf@gmail.com>
2021-02-20 21:19:20 -0300

    example

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,11 @@
 // Imports
-import users from "./users/routes.js";
+//import users from "./users/routes.js";
 // Export
 export default function(app, express){
     // Router
     const router = express.Router();
     app.use("/", router);
     // Routes
-    router.use("/users", users);
+    //router.use("/users", users);
+    router.use("/", (req,res) => res.json({sucess: true}));
 }
